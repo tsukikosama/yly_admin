325b75fd5a4b8f3e1f16ddc98656bb5a8f6e651d
weilai <1083344129@qq.com>
2024-04-11 17:24:07 +0800

    游乐园项目的完成版

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  // created: function () {
+  //   // 在页面加载时读取sessionStorage里的状态信息
+  //   sessionStorage.getItem('umeetCloudVuex') && this.$store.replaceState(Object.assign(this.$store.state, JSON.parse(sessionStorage.getItem('umeetCloudVuex'))));
+  //   // 在页面刷新时将vuex里的信息保存到sessionStorage里
+  //   window.addEventListener('beforeunload', () => {
+  //     sessionStorage.setItem('umeetCloudVuex', JSON.stringify(this.$store.state))
+  //   })
+  // }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  #app{
+    border: 0px !important;
+   margin: 0px !important;
+   width: 100% !important;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+//导入路由规则
+import router  from './router/index';
+import request from './utils/request';
+import VueRouter from 'vue-router';
+import store from './vuex/index.js'
 Vue.config.productionTip = false
 
-new Vue({
+Vue.use(VueRouter);
+Vue.use(ElementUI);
+Vue.prototype.request = request;
+
+
+ new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
+
+/**
+ * 把对象中的值清空为''
+ * @param {} obj 
+ * @returns 
+ */
+ Vue.prototype.clearValue = function(obj){
+  // console.log(obj)
+  
+  for(let key of Object.keys(obj)){       
+    console.log(key)           
+    obj[key] = '';
+    
+}
+  return obj;
+}
